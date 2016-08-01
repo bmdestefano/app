e55c3cb059f23506005dacb14ff124b87da8c37f
Brendan DeStefano <brendandestefano@Brendans-MacBook-Pro.local>
2016-08-01 18:14:37 -0400

    saving. app bar added. routes fixed

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router'
+
 import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import Profile from './components/pages/Profile';
+import Event from './components/pages/Event';
 import Main from './Main'; // Our custom react component
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-render(<Main />, document.getElementById('app'));
+render(
+	<Router history={browserHistory}>
+		<Route path="/" component={Main}>
+			<Route path="profile" component={Profile} />
+			<Route path="event" component={Event} />
+		</Route>
+	</Router>,
+document.getElementById('app'));
